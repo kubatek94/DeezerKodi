3ed9b7b3eb58896d512622a34cfb585cc8bbb986
Jakub Gawron <kubatek94@gmail.com>
2017-06-21 22:49:12 +0100

    Fixed caching behaviour

--- plugin.audio.deezer/resources/lib/cache.py
+++ plugin.audio.deezer/resources/lib/cache.py
@@ -3,21 +3,45 @@ import os.path
 import time
 import xbmc
 
+import logging
 
 class Cache(object):
-    TTL = 60 * 60  # 1 hour ttl
+    TTL = 10 * 60  # 10 mins ttl
 
     def __init__(self, name):
-        self.name = xbmc.translatePath("special://temp/%s.p" % name)
-        self.epoch = time.time()
-        if os.path.isfile(self.name):
-            self.cache = pickle.load(open(self.name, "rb"))
-        else:
-            self.cache = {}
+        self.name = name
+        self.is_loaded = False
+        self.is_dirty = False
+        self._cache = {}
+        self.file = None
+
+    def read_cache(self):
+        if not self.is_loaded:
+            path = xbmc.translatePath("special://temp/%s.p" % self.name)
+
+            if os.path.isfile(path):
+                self.file = open(path, 'r+')
+                self._cache = pickle.load(self.file)
+            else:
+                self.file = open(path, 'w+')
+                self._cache = {}
+
+            self.is_loaded = True
+        return self._cache
+
+    @property
+    def cache(self):
+        return self.read_cache()
+
+    def clear(self):
+        self.read_cache()
+        self._cache = {}
 
     def set(self, key, value, ttl=TTL):
-        key = key.lower()
-        self.cache[key] = {'time': self.epoch, 'ttl': ttl, 'value': value}
+        logging.error('set item %s', key)
+
+        self.is_dirty = True
+        self.cache[key.lower()] = {'time': time.time(), 'ttl': ttl, 'value': value}
 
     def has(self, key):
         key = key.lower()
@@ -25,14 +49,17 @@ class Cache(object):
             return False
         else:
             item = self.cache[key]
-            # if item lives longer than ttl allows, remove it from cache
-            if (item['time'] + item['ttl']) > self.epoch:
+            # if item's expiry time is in the past, remove it from cache
+            if item['time'] + item['ttl'] < time.time():
+                self.is_dirty = True
                 del self.cache[key]
                 return False
             else:
                 return True
 
     def get(self, key, default_value=None, default_producer=None, ttl=TTL):
+        logging.debug('read item %s', key)
+        key = key.lower()
         if self.has(key):
             return self.cache[key]['value']
         if default_producer is not None:
@@ -42,4 +69,6 @@ class Cache(object):
         return default_value
 
     def save(self):
-        pickle.dump(self.cache, open(self.name, "wb"))
+        if self.is_dirty and self.file is not None:
+            self.file.seek(0)
+            pickle.dump(self.cache, self.file, protocol=-1)  # -1 will choose fastest version supported
